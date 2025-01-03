6004108e017a5468cbad00ca7dd5982775b6a18f
Rachitlad24 <rachitlad.it21@scet.ac.in>
2025-01-03 13:14:28 +0530

    newfunc added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const name="rachit";
+const name1="rachit";
+const age=22;
+const val=35;
+function sayname(){
+  console.log('my name is + name1')  
+}
+ function hello(){
+    console.log("helllo")
+ }
+ 
